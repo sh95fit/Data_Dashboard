a26aaa492f2a137b6e516be9585afd9f068ae37a
sh95fit <h95fit@gmail.com>
2024-01-04 08:59:50 +0900

    FastAPI, React - Docker, Docker-compose 초기 세팅

---
+++ Backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def main():
+    return "hello"
+
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000",
+                   "http://127.0.0.1:3000"],  # 띄우는 리액트 앱의 주소에 맞게 수정
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
