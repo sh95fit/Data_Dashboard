42ffe5f25490df127f8bbd0e2838213d23c1e0bd
sh95fit <h95fit@gmail.com>
2024-11-25 09:48:33 +0900

    favicon 동적 업데이트 적용 완료 (react-helmet => react-helmet-async 전환)

--- Frontend/dashboard/src/ikea/pages/IkeaDashboard.jsx
+++ Frontend/dashboard/src/ikea/pages/IkeaDashboard.jsx
@@ -1,27 +1,9 @@
-import React, { useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
+import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Media from '../components/Media'
 // import Comment from '../components/Comment'
 
-import { Helmet } from 'react-helmet';
-
-
-// const IkeaFavicon = () => {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     if (location.pathname === '/ikea') {
-//       document.title = "IKEA";
-//       const favicon = document.querySelector("link[rel='icon']");
-//       if (favicon) {
-//         favicon.href = "/favicon/ikea/favicon.ico"
-//       }
-//     }
-//   }, [location]);
-
-//   return null;
-// }
+import { Helmet } from 'react-helmet-async';
 
 
 const IkeaDashboard = () => {
@@ -29,8 +11,22 @@ const IkeaDashboard = () => {
     <div className="grid h-screen bg-gray-100 grid-cols-20 grid-rows-12">
 
       <Helmet>
-        <title>Ikea</title>
-        <link rel="icon" href="/favicon/ikea/favicon.ico" type="image/x-icon" />
+        <title>IKEA</title>
+        <link rel="icon" href="/favicon/ikea/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/ikea/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/ikea/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/ikea/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/ikea/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/ikea/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/ikea/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/ikea/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/ikea/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/ikea/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/ikea/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/ikea/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/ikea/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/ikea/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon/ikea/manifest.json"/>
       </Helmet>
 
 
--- Frontend/dashboard/src/main.jsx
+++ Frontend/dashboard/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
 
@@ -9,7 +10,9 @@ import store from "./store";
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <Provider store={store}>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </Provider>
   // </StrictMode>,
 )
